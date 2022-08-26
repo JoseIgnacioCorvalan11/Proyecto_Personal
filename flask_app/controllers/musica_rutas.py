6dc2c95e09b0d4cab0258c1e717c4065bb269345
jose ignacio <joseignaciocorva@gmail.com>
2022-08-25 20:48:52 -0400

    proyecto_solo

---
+++ flask_app/controllers/musica_rutas.py
@@ -0,0 +1,71 @@
+from flask_app.models.registro_login import Proyecto
+from flask_bcrypt import Bcrypt   
+from flask_app import app
+bcrypt = Bcrypt(app) 
+from flask_app import app
+from flask import render_template,redirect,request,session,flash
+
+
+
+@app.route('/main', methods=['GET'])
+def inicio_sesion_registro():
+    return render_template('register_login.html')
+
+@app.route('/dashboard', methods=['GET'])
+def ingresado_exitosamente():
+    if 'id' not in session:
+        return redirect('/main')
+    data = {
+        "id": session['id']
+    }
+    return render_template('music_dashboard.html')
+
+
+@app.route('/registrarse', methods=['POST'])
+def registro():
+    if request.method == 'POST':
+        print(request.form, "/?/"*20)
+        if not Proyecto.validar_registro(request.form):
+            flash("Registro invalido")
+            return redirect('/main')
+        data = {
+            "name" : request.form['name'],
+            "last_name" : request.form['last_name'],
+            "email" : request.form['email'],
+            "password" : bcrypt.generate_password_hash(request.form['password']),
+            "confirm_password" : request.form['confirm_password'],
+            "date" : request.form['date']
+        }
+        print(data, "/*-"*20)
+        usuario_id = Proyecto.save(data)
+        print(data)
+        session["id"] = usuario_id 
+        return redirect("/dashboard")
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['email'])
+        usuario = Proyecto.getEmail(request.form)
+        if not usuario:
+            flash("Este dato esta erroneo")
+            return redirect("/main")
+        if usuario == False or not bcrypt.check_password_hash(usuario.password, request.form['password']):
+            flash("Contraseña erronea")
+            return redirect("/main")
+        session["id"] = usuario.id
+        print(usuario)
+        return redirect('/dashboard')
+
+@app.route('/destroy')
+def destroy():
+    session.clear()
+    return redirect('/main')
+
+@app.route('/user/<int:id>', methods=['GET'])
+def view(id):
+    data = {
+        "id": id
+    }
+    view_all = Proyecto.get_one(data)
+    return render_template('view_user_music.html', view_all=view_all)
---
+++ flask_app/models/registro_login.py
@@ -0,0 +1,95 @@
+import datetime
+from flask_app import app     
+import re
+from flask import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+DIGITO_REGEX = re.compile(r'\d+')
+MAYUSCULA_REGEX = re.compile(r'[A-Z]+')
+
+class Proyecto:
+    def __init__(self, data ):
+        self.id = data['id']
+        self.name = data['name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @staticmethod
+    def validar_registro(formulario):
+        fecha_actual = datetime.datetime.now()
+
+        valido = True
+        if not EMAIL_REGEX.match(formulario['email']):
+            flash("Email no valido! ERROR ERROR!!!")
+            valido = False
+        if len(formulario['name']) < 3:
+            flash("Nombre debe ser al menos 3 caracteres largos")
+            valido = False
+        if len(formulario['last_name']) < 3:
+            flash("Nombre debe ser al menos 3 caracteres largos")
+            valido = False
+        if not DIGITO_REGEX.search(formulario['password']):
+            flash("Se necesita al menos 1 digito")
+            valido = False
+        if not MAYUSCULA_REGEX.search(formulario['password']):
+            flash("Se necesita al menos 1 Mayuscula")
+            print(formulario['password'])
+            valido = False
+        # if int(formulario['radio']) == 1:
+        #     valido = True
+        if len(formulario['date']) < 1:
+            print(fecha_actual, "FECHA ACTUAL"*10)
+            flash("Por favor llene su cumple")
+            valido = False
+        if len(formulario['date']) > 1:
+            fecha_formulario = datetime.datetime.strptime(formulario["date"], "%Y-%m-%d")
+            total_fecha =  fecha_actual - fecha_formulario
+            convertirfechaenINT = int(total_fecha.days)
+            print(convertirfechaenINT, "TOTAL RESTAR FECHAS"*10)
+            if convertirfechaenINT < 6570:
+                flash("Usuario debe ser mayor de 18")
+                valido = False
+        return valido
+
+    @classmethod
+    def save(cls, data):
+        query="INSERT INTO registro_login (name , last_name, email, password) VALUES ( %(name)s , %(last_name)s , %(email)s , %(password)s );"
+        return connectToMySQL('schema_musica').query_db( query, data )
+
+    @classmethod
+    def get_all(cls, id):
+        query= "SELECT * FROM registro_login WHERE id != %(id)s;"
+        results = connectToMySQL('schema_musica').query_db(query, id)
+        print("QUE HAY AQUI?", results)
+        proyecto_result = []
+        for proyecto in results:
+            proyecto_result.append( cls(proyecto) )
+        return proyecto_result
+
+    @classmethod
+    def getEmail(cls, data):
+        print(data, "Esto esta en el data")
+        query = "SELECT * FROM registro_login WHERE email = %(email)s;"
+        results = connectToMySQL('schema_musica').query_db( query, data )
+        if len(results) < 1:
+            return False
+        print("RETORNO GET EMAIL:",results)
+        return cls(results[0])
+
+    @classmethod
+    def delete(cls, data):
+        query= "DELETE FROM registro_login WHERE id = %(id)s;"
+        return connectToMySQL('schema_musica').query_db( query, data )
+
+    @classmethod
+    def get_one(cls, data):
+        query= "SELECT * FROM registro_login WHERE id = %(id)s;"
+        results = connectToMySQL('schema_musica').query_db(query, data)
+        print("QUE HAY AQUI?", results)
+        if len(results) < 1:
+            return False
+        print("RETORNO GET EMAIL:",results)
+        return cls(results[0])
